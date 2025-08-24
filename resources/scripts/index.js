01ac6ecd51ee09c6de3edf80310620e6ff439354
jordanjt4 <jordan.jt4@comcast.net>
2025-08-23 22:41:59 -0700

    edited font size

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -3,15 +3,17 @@ const cloud2 = document.getElementById('cloud2');
 
 function updateClouds () {
     let value = window.scrollY;
-    let fade = window.screen.width;
-    let opacity = value/fade;
+    let width = window.screen.width;
+    let shift = Math.min(value * 0.5, width * 0.2);
+    let opacity = (value * 2)/width;
 
-    if (window.innerWidth > 300) {
-        cloud1.style.transform = `translateX(${value * 0.5}px)`;
-        cloud1.style.opacity = Math.min(opacity, 1);
-        cloud2.style.transform = `translateX(${-value * 0.5}px)`;
+    cloud1.style.transform = `translateX(${shift}px)`;
+    cloud1.style.opacity = Math.min(opacity, 1);
+
+    if (window.innerWidth > 500) {
+        cloud2.style.transform = `translateX(${-shift}px)`;
         cloud2.style.opacity = Math.min(opacity, 1);
-    }
+    } 
 }
 
 document.addEventListener('scroll', updateClouds);
